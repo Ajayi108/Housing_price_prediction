d7d858fdfb7470d91df91c056398383b8c86e729
Ikponmwosa Ajayi <ikponmwosa.ajayi@stud.th-deg.de>
2025-05-27 17:03:31 +0200

    Home page for streamlit

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Housing App", page_icon="🏠", layout="wide")
+
+st.title("Housing Price Prediction and Chatbot")
+st.sidebar.success("Select a page above.")
+
+st.write("Welcome to the housing price prediction app!")
+st.write("Use the sidebar to navigate between pages.")
+st.write("I am a chatbot that can make predictions of housing price for 188 cities in the USA:")
